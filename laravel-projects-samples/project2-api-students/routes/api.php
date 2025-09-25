d38c63da04e02dc0e1522e2b7bc49613e87aa5bb
alhines <36683112+alhines@users.noreply.github.com>
2025-09-25 11:50:59 -0600

    Delete laravel-projects-samples directory

--- laravel-projects-samples/project2-api-students/routes/api.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Api\EstudianteController;
-
-Route::get   ('/estudiantes',                [EstudianteController::class, 'index']);
-Route::post  ('/estudiantes',                [EstudianteController::class, 'store']);
-Route::match (['put','patch'], '/estudiantes/{estudiante}', [EstudianteController::class, 'update']);
-Route::delete('/estudiantes/{estudiante}',   [EstudianteController::class, 'destroy']);
